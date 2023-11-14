6c874eb296312917ed452676660c4cd76d897264
Cao Khánh Hưng <71182720+caohung131@users.noreply.github.com>
2023-11-14 23:04:22 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
-const userRouter = require('./users.js');
+const productRouter = require('./product.js');
+const userRouter = require('./user.js');
+const orderRouter = require('./order.js');
 var router = express.Router();
 
+router.use('/products', productRouter)
 router.use('/users', userRouter)
+router.use('/orders', orderRouter)
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const { login } = require('../src/controlers');
 var userRouter = express.Router();
 
 /* GET users listing. */
-userRouter.get('/', () => {
-  console.log('aa')
-})
+userRouter.post('/login', login),
 
 module.exports = userRouter;
